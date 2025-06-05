f8a5d82bcd5687920e077338f1b689ef2c024a65
Hite9370 <131429610+Hite9370@users.noreply.github.com>
2025-06-05 20:35:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-  $(document).ready(function () {
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 2,
-      centeredSlides: true,
-      loop: true,      
-      spaceBetween: 24,
-      slideToClickedSlide: true,
+$(document).ready(function() {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 24,
+    slideToClickedSlide: true,
       breakpoints: {
         320: {
           spaceBetween: 16,
         }
       }
-    });
   });
+});
+
