f4052077004dce91b08bf1dee0e57ee057239da9
Hite9370 <131429610+Hite9370@users.noreply.github.com>
2025-11-25 23:49:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -59,14 +59,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   const swiper = new Swiper(".swiper", {
-    slidesPerView: "auto",
+    slidesPerView: 3,
     spaceBetween: 20,
-    freeMode: true,
     navigation: {
       nextEl: ".button-next",
       prevEl: ".button-prev",
     },
   });
+
 
 
 
